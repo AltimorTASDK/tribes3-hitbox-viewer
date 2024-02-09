f026a0c8e9a30296a04df08de3b9411446b0ee49
AltimorTASDK <altimormc@gmail.com>
2024-02-08 22:37:44 -0500

    working on t3 port

--- hitboxes.js
+++ hitboxes.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const ROTATION_SENSITIVITY = 0.3;
+
 let renderer = null;
 let camera = null;
 let camera2d = null;
@@ -9,22 +11,17 @@ let hitboxes = [];
 
 let heroScene = null;
 let hitboxScene = null;
-let headHitboxScene = null;
 let screenScene = null;
 
 let heroRt = null;
 let hitboxRt = null;
-let headHitboxRt = null;
-
-let hitboxMaterial = null;
-let headHitboxMaterial = null;
 
 let cylinderGeom = null;
 let halfSphereGeom = null;
 let sphereGeom = null;
 let boxGeom = null;
 
-let cameraRotation = { Pitch: 0, Yaw: 0, Roll: 0 };
+let cameraRotation = {Pitch: 0, Yaw: 0, Roll: 0};
 let dragging = false;
 
 function hamiltonProduct(a, b)
@@ -47,12 +44,12 @@ function rotateVectorByQuaternion(vector, rotation)
     let point = {X: vector.X, Y: vector.Y, Z: vector.Z, W: 0};
     let inverse = quaternionInverse(rotation);
     let rotated = hamiltonProduct(hamiltonProduct(rotation, point), inverse);
-    return { X: rotated.X, Y: rotated.Y, Z: rotated.Z };
+    return {X: rotated.X, Y: rotated.Y, Z: rotated.Z};
 }
 
 function vectorAdd(a, b)
 {
-    return { X: a.X + b.X, Y: a.Y + b.Y, Z: a.Z + b.Z };
+    return {X: a.X + b.X, Y: a.Y + b.Y, Z: a.Z + b.Z};
 }
 
 function rotatorToQuat(rotator)
@@ -106,70 +103,49 @@ function createSphyl(scene, material, start, end, radius)
     scene.add(endCap);
 }
 
-function createHitbox(scene, material, hitbox, bone)
+function createHitbox(scene, material, hitbox)
 {
-    for (const elem of hitbox.AggGeom.SphylElems ?? []) {
-        const rotation = rotatorToQuat(elem.Rotation);
-        const offset1 = vectorAdd(rotateVectorByQuaternion(
-            {X: 0, Y: 0, Z:  elem.Length / 2}, rotation), elem.Center);
-        const offset2 = vectorAdd(rotateVectorByQuaternion(
-            {X: 0, Y: 0, Z: -elem.Length / 2}, rotation), elem.Center);
-        const position1 = boneTransform(bone, offset1);
-        const position2 = boneTransform(bone, offset2);
-        createSphyl(scene, material, position1, position2, elem.Radius);
-    }
+    if (hitbox.BoneName !== "upperarm_l" && hitbox.BoneName !== "upperarm_r")
+        return;
 
-    for (const elem of hitbox.AggGeom.SphereElems ?? []) {
+    const bone = bones[hitbox.BoneName];
+
+    hitbox.AggGeom.SphylElems?.forEach(elem => {
+        const rotation = rotatorToQuat(elem.Rotation);
+        const offset1 = rotateVectorByQuaternion({X: 0, Y: 0, Z:  elem.Length / 2}, rotation);
+        const offset2 = rotateVectorByQuaternion({X: 0, Y: 0, Z: -elem.Length / 2}, rotation);
+        const position1 = boneTransform(bone, vectorAdd(offset1, elem.Center));
+        const position2 = boneTransform(bone, vectorAdd(offset2, elem.Center));
+        createSphyl(scene, material, position1, position2, elem.Radius);
+    });
+
+    hitbox.AggGeom.SphereElems?.forEach(elem => {
         const center = boneTransform(bone, elem.Center);
         const sphere = new THREE.Mesh(sphereGeom, material);
         sphere.scale.set(elem.Radius, elem.Radius, elem.Radius);
         sphere.position.set(center.X, center.Y, center.Z);
         scene.add(sphere);
-    }
+    });
 
-    for (const elem of hitbox.AggGeom.BoxElems ?? []) {
+    hitbox.AggGeom.BoxElems?.forEach(elem => {
         const center = boneTransform(bone, elem.Center);
         const box = new THREE.Mesh(boxGeom, material);
         box.scale.set(elem.X, elem.Y, elem.Z);
         bone.getWorldQuaternion(box.quaternion);
-        box.rotateX(elem.Rotation.Pitch * Math.PI / 180 + Math.PI / 2);
-        box.rotateY(elem.Rotation.Yaw * Math.PI / 180);
-        box.rotateZ(elem.Rotation.Roll * Math.PI / -180);
+        box.rotateX(elem.Rotation.Pitch *  Math.PI / 180 + Math.PI / 2);
+        box.rotateY(elem.Rotation.Yaw   *  Math.PI / 180);
+        box.rotateZ(elem.Rotation.Roll  * -Math.PI / 180);
         box.position.set(center.X, center.Y, center.Z);
         scene.add(box);
-    }
-}
-
-function drawHitboxes(rt, drawHead)
-{
-    renderer.setRenderTarget(rt);
-    renderer.render(hitboxScene, camera);
-    renderer.setRenderTarget(null);
-    return rt;
+    });
 }
 
 function updateBones(object)
 {
+    if (object.type == "Bone")
+        bones[object.name] = object;
+
     object.children.forEach(updateBones);
-
-    if (object.type != "Bone")
-        return;
-
-    const position = new THREE.Vector3();
-    const quaternion = new THREE.Quaternion();
-    object.getWorldPosition(position);
-    object.getWorldQuaternion(quaternion);
-    quaternion.multiply(new THREE.Quaternion(Math.SQRT1_2, 0, 0, Math.SQRT1_2));
-    bones[object.name] = object;
-}
-
-function drawHero(rt)
-{
-    renderer.setRenderTarget(rt);
-    renderer.render(heroScene, camera);
-    renderer.setRenderTarget(null);
-
-    return rt;
 }
 
 function renderLoop()
@@ -179,9 +155,6 @@ function renderLoop()
 
     renderer.setRenderTarget(hitboxRt);
     renderer.render(hitboxScene, camera);
-
-    renderer.setRenderTarget(headHitboxRt);
-    renderer.render(headHitboxScene, camera);
 
     renderer.setRenderTarget(null);
     renderer.render(screenScene, camera2d);
@@ -205,16 +178,12 @@ function createHeroScene()
     return scene;
 }
 
-function createHitboxScene(drawHead)
+function createHitboxScene()
 {
     const scene = new THREE.Scene();
     addLights(scene);
-
-    for (const hitbox of hitboxes.filter(h => ["Head", "Neck"].includes(h.BoneName) == drawHead)) {
-        const material = drawHead ? headHitboxMaterial : hitboxMaterial;
-        createHitbox(scene, material, hitbox, bones[hitbox.BoneName]);
-    }
-
+    const material = new THREE.MeshStandardMaterial({color: 0x007FFF});
+    hitboxes.forEach(hitbox => createHitbox(scene, material, hitbox));
     return scene;
 }
 
@@ -223,26 +192,17 @@ function createScreenScene()
     const scene = new THREE.Scene();
     const geometry = new THREE.PlaneGeometry(100, 100);
 
-    const characterMat = new THREE.MeshBasicMaterial({map: heroRt.texture});
-    const characterQuad = new THREE.Mesh(geometry, characterMat);
+    // character
+    scene.add(new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+        map: heroRt.texture,
+    })));
 
-    const hitboxMat = new THREE.MeshBasicMaterial({
+    // hitboxes
+    scene.add(new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
         map: hitboxRt.texture,
         opacity: 0.5,
-        transparent: true
-    });
-    const hitboxQuad = new THREE.Mesh(geometry, hitboxMat);
-
-    const headMat = new THREE.MeshBasicMaterial({
-        map: headHitboxRt.texture,
-        opacity: 0.5,
-        transparent: true
-    });
-    const headQuad = new THREE.Mesh(geometry, headMat);
-
-    scene.add(characterQuad);
-    scene.add(hitboxQuad);
-    scene.add(headQuad);
+        transparent: true,
+    })));
 
     return scene;
 }
@@ -264,9 +224,6 @@ function renderInit()
     renderer.outputEncoding = THREE.sRGBEncoding;
     document.body.appendChild(renderer.domElement);
 
-    hitboxMaterial = new THREE.MeshStandardMaterial({color: 0x007FFF});
-    headHitboxMaterial = new THREE.MeshStandardMaterial({color: 0xFF00FF});
-
     cylinderGeom = new THREE.CylinderGeometry(1, 1, 1, 32, 1);
     halfSphereGeom = new THREE.SphereGeometry(1, 16, 16, Math.PI, Math.PI);
     sphereGeom = new THREE.SphereGeometry(1, 16, 16);
@@ -274,18 +231,17 @@ function renderInit()
 
     heroRt = new THREE.WebGLRenderTarget(width, height);
     hitboxRt = new THREE.WebGLRenderTarget(width, height);
-    headHitboxRt = new THREE.WebGLRenderTarget(width, height);
 
     heroScene = createHeroScene();
-    hitboxScene = createHitboxScene(false);
-    headHitboxScene = createHitboxScene(true);
+    hitboxScene = createHitboxScene();
     screenScene = createScreenScene();
 }
 
 function startRendering()
 {
     const loader = new THREE.GLTFLoader();
-    loader.load("models/MESH_PC_BloodEagleLight_A.glb",
+
+    loader.load("/models/MESH_PC_BloodEagleLight_A.glb",
         gltf => {
             gltf.scene.scale.set(100, 100, 100);
             gltf.scene.rotation.x = Math.PI / 2;
@@ -293,9 +249,12 @@ function startRendering()
             updateBones(gltf.scene);
             heroModel = gltf.scene;
 
-            $.getJSON("/json/Core_HitboxPhysicsAsset2.json", data => {
-                //hitboxes = Object.values(data)[0].SkeletalBodySetups.map(k => data[k]);
-                hitboxes = {};
+            $.getJSON("/json/SK_Mannequin_PhysicsAsset_Light.json", data => {
+                hitboxes = data[0].Properties.SkeletalBodySetups.map(obj => {
+                    const split = obj.ObjectPath.split('.');
+                    const index = split[split.length - 1];
+                    return data[index].Properties;
+                });
                 renderInit();
                 renderLoop();
             });
@@ -319,8 +278,8 @@ document.onmousemove = function(event)
     if (camera == null || !(event.buttons & 1))
         return;
 
-    cameraRotation.Yaw -= event.movementX * 0.3;
-    cameraRotation.Pitch += event.movementY * 0.3;
+    cameraRotation.Yaw   -= event.movementX * ROTATION_SENSITIVITY;
+    cameraRotation.Pitch += event.movementY * ROTATION_SENSITIVITY;
     updateCamera();
 };
 
